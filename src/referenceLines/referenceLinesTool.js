4ba52ab53969c49495cf45deda92bb195de5adbe
Jose Antonio Rapozo <jose.rezende@mv.com.br>
2018-06-22 17:26:23 -0300

    Only show reference lines of selected viewport;

--- src/referenceLines/referenceLinesTool.js
+++ src/referenceLines/referenceLinesTool.js
@@ -1,6 +1,6 @@
 import EVENTS from '../events.js';
 import external from '../externalModules.js';
-import { addToolState, getToolState } from '../stateManagement/toolState.js';
+import { addToolState, getToolState, clearToolState } from '../stateManagement/toolState.js';
 import renderActiveReferenceLine from './renderActiveReferenceLine.js';
 
 const toolType = 'referenceLines';
@@ -17,9 +17,10 @@ function onImageRendered (e) {
 
   // Get the enabled elements associated with this synchronization context and draw them
   const syncContext = toolData.data[0].synchronizationContext;
-  const enabledElements = syncContext.getSourceElements();
-
+  const activeElement = toolData.data[0].activeElement;
   const renderer = toolData.data[0].renderer;
+
+  const enabledElements = syncContext.getSourceElements();
 
   // Create the canvas context and reset it to the pixel coordinate system
   const context = eventData.canvasContext.canvas.getContext('2d');
@@ -34,6 +35,9 @@ function onImageRendered (e) {
       return;
     }
 
+    if (activeElement && (activeElement !== referenceEnabledElement)) {
+      return;
+    }
     // Render it
     renderer(context, eventData, e.currentTarget, referenceEnabledElement);
   });
@@ -41,17 +45,24 @@ function onImageRendered (e) {
 
 // Enables the reference line tool for a given element.  Note that a custom renderer
 // Can be provided if you want different rendering (e.g. all reference lines, first/last/active, etc)
-function enable (element, synchronizationContext, renderer) {
+function enableSingleReferenceLine (element, activeElement, synchronizationContext, renderer) {
   renderer = renderer || renderActiveReferenceLine;
+
+  clearToolState(element, toolType);
 
   addToolState(element, toolType, {
     synchronizationContext,
-    renderer
+    renderer,
+    activeElement
   });
 
   element.removeEventListener(EVENTS.IMAGE_RENDERED, onImageRendered);
   element.addEventListener(EVENTS.IMAGE_RENDERED, onImageRendered);
   external.cornerstone.updateImage(element);
+}
+
+function enable (element, synchronizationContext, renderer) {
+  enableSingleReferenceLine(element, undefined, synchronizationContext, renderer);
 }
 
 // Disables the reference line tool for the given element
@@ -63,6 +74,7 @@ function disable (element) {
 // Module/private exports
 const tool = {
   enable,
+  enableSingleReferenceLine,
   disable
 };
 
